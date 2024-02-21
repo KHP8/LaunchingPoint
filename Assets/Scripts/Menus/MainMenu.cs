a4fb6595bc0144921ea5a7592c399a8ef5e32759
Austin Vosler <austinpvosler@gmail.com>
2024-02-20 23:54:29 -0500

    Updated Settings Menu: Display Tab
    Display Mode and Resolution settings added.
    
    The resolutions array auto populates by grabbing every available resolution from Screen.resolutions.

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -7,18 +7,19 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public GameObject newsMenu;
-    private bool newsMenuOpen = false;
+    //private bool newsMenuOpen;
     public GameObject[] bannerButtons = new GameObject[7];
 
     public void Start()
     {
         newsMenu.SetActive(false);
+        //newsMenuOpen = false;
     }
 
     public void OnNewsButton()
     {
         newsMenu.SetActive(true);
-        newsMenuOpen = true;
+        //newsMenuOpen = true;
         for (int i = 0; i < bannerButtons.Length; i++)
         {
             bannerButtons[i].GetComponent<Button>().enabled = false;
@@ -28,7 +29,7 @@ public class MainMenu : MonoBehaviour
     public void OnCloseNewsButton()
     {
         newsMenu.SetActive(false);
-        newsMenuOpen = false;
+        //newsMenuOpen = false;
         for (int i = 0; i < bannerButtons.Length; i++)
         {
             bannerButtons[i].GetComponent<Button>().enabled = true;
