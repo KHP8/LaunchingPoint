8be4c103281a74074f7705c262a52197d5566d4b
Elephoont <haydenhuening@icloud.com>
2024-03-05 22:59:07 -0500

    Physics Based Movement
    Completely rewrote the PlayerMotor script to work on appyling forces to a rigid body for more consistent and natural movement. Should be easier to implement future movement features with this method. Values need to be tweaked to have movement feeling fast and responsive but it is promising.

--- Assets/Scripts/Player/PlayerMotor.cs
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerMotor : MonoBehaviour
 {
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool isGrounded;
+    private Rigidbody playerRigidbody;
+    public Transform orientation;
+    private Vector3 moveDirection;
+
+    [Header("Grounded Check")]
+    public float playerHeight = 2f;
+    public LayerMask whatIsGround;
+    public bool isGrounded;
 
     [Header("Constants")]
     public float speed = 5f;
     public float gravity = -9.8f;
     public float jumpHeight = 0.75f;
+    public float groundDrag = 0.5f;
+    public float jumpCooldown = 0.2f;
+    public float airMultiplier = 0.25f;
+    public bool readyToJump;
+
+    public float speedDisplay;
 
     private bool crouching = false;
     private float crouchTimer = 1;
@@ -30,13 +43,27 @@ public class PlayerMotor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerRigidbody.freezeRotation = true;
+        ResetJump();
     }
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = controller.isGrounded;
+        // Grounded Check
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        // Apply ground drag
+        if (isGrounded)
+            playerRigidbody.drag = groundDrag;
+        else
+            playerRigidbody.drag = 0;
+
+        //limit speed
+        SpeedControl();
+
+        /*isGrounded = controller.isGrounded;
         if (lerpCrouch)
         {
             crouchTimer += Time.deltaTime;
@@ -56,44 +83,51 @@ public class PlayerMotor : MonoBehaviour
 
         // check if needs to be respawned
         if (controller.transform.position[1] < minHeight) {
-            Debug.Log(controller.transform.position);
+            UnityEngine.Debug.Log(controller.transform.position);
             controller.transform.position = spawnPoint.position;
+        }*/
+    }
+
+
+    public void ProcessMove(Vector2 input)
+    {
+        moveDirection = orientation.forward * input.y + orientation.right * input.x;
+        
+        //grounded
+        if(isGrounded)
+            playerRigidbody.AddForce(moveDirection.normalized * speed, ForceMode.Force);
+        //in air
+        else if(!isGrounded)
+            playerRigidbody.AddForce(moveDirection.normalized * speed * airMultiplier, ForceMode.Force);
+
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(playerRigidbody.velocity.x, 0f, playerRigidbody.velocity.z);
+        speedDisplay = flatVel.magnitude;
+        //limit velocity
+        if(flatVel.magnitude > speed)
+        {
+            Vector3 limitedVel = flatVel.normalized * speed;
+            playerRigidbody.velocity = new Vector3(limitedVel.x, playerRigidbody.velocity.y, limitedVel.x);
         }
-    }
-
-    // Receive the inputs for our InputManager.cs and apply them to our character controller.
-    public void ProcessMoveOld( Vector2 input )
-    {
-        Vector3 moveDirection = Vector3.zero;
-        moveDirection.x = input.x;
-        moveDirection.z = input.y;
-        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
-        playerVelocity.y += gravity * Time.deltaTime;
-        if(isGrounded && playerVelocity.y < 0)
-            playerVelocity.y = -2f;
-        controller.Move(playerVelocity * Time.deltaTime);
-    }
-
-    public void ProcessMove( Vector2 input) // bugs: one input stops all movement when jumping, but not two inputs? momentum is carried through based on player rotation. A fixed global position would be better
-    {
-        Vector3 moveDirection = Vector3.zero;
-        moveDirection.x = (!controller.isGrounded && input.x == 0) ? 0.8f * Mathf.Sqrt(Mathf.Abs(lastxInput)) * (lastxInput/Mathf.Abs(lastxInput)) : input.x; //if the player is in the air and not inputting any directions, slow them down to keep momentum
-        moveDirection.z = (!controller.isGrounded && input.y == 0) ? 0.8f * Mathf.Sqrt(Mathf.Abs(lastzInput)) * (lastzInput/Mathf.Abs(lastzInput)) : input.y;
-        lastxInput = moveDirection.x;
-        lastzInput = moveDirection.z;
-        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
-        playerVelocity.y += gravity * Time.deltaTime;
-        if (isGrounded && playerVelocity.y < 0)
-            playerVelocity.y = -2f;
-        controller.Move(playerVelocity * Time.deltaTime);
     }
 
     public void Jump()
     {
-        if(isGrounded) 
-        {
-            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        if(isGrounded && readyToJump) 
+        {   
+            playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x, 0f, playerRigidbody.velocity.z);
+            playerRigidbody.AddForce(orientation.up * 10f * jumpHeight, ForceMode.Impulse);
+            readyToJump = false;
+            Invoke(nameof(ResetJump), jumpCooldown);
         }
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 
     public void Crouch()
@@ -107,8 +141,8 @@ public class PlayerMotor : MonoBehaviour
     {
         sprinting = !sprinting;
         if (sprinting)
-            speed = 8;
+            speed = 13f;
         else
-            speed = 5;
+            speed = 10f;
     }
 }
