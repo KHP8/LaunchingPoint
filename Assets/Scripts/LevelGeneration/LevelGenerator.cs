715a47765d9c1a75c6e9fb7594932afad671b71f
Austin Vosler <austinpvosler@gmail.com>
2024-04-03 01:12:16 -0400

    Teleports to correct location, incorrect rotation.

--- Assets/Scripts/LevelGeneration/LevelGenerator.cs
+++ Assets/Scripts/LevelGeneration/LevelGenerator.cs
@@ -15,20 +15,23 @@ public class LevelGenerator : MonoBehaviour
     {
         roomList = Resources.LoadAll("Prefabs/RoomsReal").Cast<GameObject>().ToList();
 
+        GameObject[] rooms = new GameObject[4];
+
         for (int i = 0; i < 4; i++)
         {
             GameObject room = roomList[i];
             Vector3 position = new Vector3(0, 0, 250 * (i + 1));
             Quaternion rotation = new Quaternion(0, 0, 0, 0);
-            if (i != 3)
-                room.GetComponentInChildren<LevelExit>().nextRoom = roomList[i + 1];
-            else
-                room.GetComponentInChildren<LevelExit>().nextRoom = roomList[0];
+            if (i > 1)
+                rooms[i - 2].GetComponentInChildren<LevelExit>().nextRoom = rooms[i - 1];
+
             room.GetComponent<RoomHandler>().player = player;
             room.GetComponent<RoomHandler>().virtualCamera = virtualCamera;
-            Instantiate(room, position, rotation);
-
+            rooms[i] = Instantiate(room, position, rotation);
         }
+            rooms[2].GetComponentInChildren<LevelExit>().nextRoom = rooms[3];
+            rooms[3].GetComponentInChildren<LevelExit>().nextRoom = rooms[0];
+
 
         virtualCamera
             .GetComponent<CinemachineVirtualCamera>()
