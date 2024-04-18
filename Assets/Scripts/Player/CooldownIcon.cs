90c7b90ea30b551e306bbc8c9ccaf761116e39d8
Austin Vosler <austinpvosler@gmail.com>
2024-04-18 03:41:07 -0400

    Updating ability icon cooldown graphics

--- Assets/Scripts/Player/CooldownIcon.cs
+++ Assets/Scripts/Player/CooldownIcon.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CooldownIcon : MonoBehaviour
 {
     public float cooldown;
     public GameObject textBox;
+    public GameObject icon;
     private bool canStartTimer = true;
 
     public void CooldownSelector(float abilityCooldown)
     {
         cooldown = abilityCooldown;
-        StartCoroutine(UpdateAbilityTimer());
-
+        StartCoroutine(UpdateAbilityTimerImage());
     }
 
     public IEnumerator UpdateAbilityTimer()
@@ -27,6 +28,23 @@ public class CooldownIcon : MonoBehaviour
                 yield return new WaitForSeconds(.1f);
             }
             textBox.GetComponent<TextMeshProUGUI>().text = "";
+            canStartTimer = true;
+        }
+    }
+
+    public IEnumerator UpdateAbilityTimerImage()
+    {
+        float percent = 0;
+        icon.GetComponent<Image>().fillAmount = percent;
+        if (canStartTimer)
+        {
+            canStartTimer = false;
+            for (float i = 0; i < cooldown; i += .1f)
+            {
+                percent += (1 / (cooldown * 10));
+                icon.GetComponent<Image>().fillAmount = percent;
+                yield return new WaitForSeconds(.1f);
+            }
             canStartTimer = true;
         }
     }
