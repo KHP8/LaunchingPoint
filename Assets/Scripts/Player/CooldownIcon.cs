c333599bbeb0ccb7c6bbb323f7d9d2b1121f9ff9
Austin Vosler <austinpvosler@gmail.com>
2024-04-19 05:52:02 -0400

    New tablet icon

--- Assets/Scripts/Player/CooldownIcon.cs
+++ Assets/Scripts/Player/CooldownIcon.cs
@@ -14,7 +14,7 @@ public class CooldownIcon : MonoBehaviour
     public void CooldownSelector(float abilityCooldown)
     {
         cooldown = abilityCooldown;
-        StartCoroutine(UpdateAbilityTimerImage());
+        StartCoroutine(UpdateAbilityTimerImageUp());
     }
 
     public IEnumerator UpdateAbilityTimer()
@@ -32,7 +32,7 @@ public class CooldownIcon : MonoBehaviour
         }
     }
 
-    public IEnumerator UpdateAbilityTimerImage()
+    public IEnumerator UpdateAbilityTimerImageDown()
     {
         float percent = 1;
         icon.GetComponent<Image>().fillAmount = percent;
@@ -42,6 +42,23 @@ public class CooldownIcon : MonoBehaviour
             for (float i = cooldown; i >= 0; i -= .1f)
             {
                 percent -= (1 / (cooldown * 10));
+                icon.GetComponent<Image>().fillAmount = percent;
+                yield return new WaitForSeconds(.1f);
+            }
+            canStartTimer = true;
+        }
+    }
+
+    public IEnumerator UpdateAbilityTimerImageUp()
+    {
+        float percent = 0;
+        icon.GetComponent<Image>().fillAmount = percent;
+        if (canStartTimer)
+        {
+            canStartTimer = false;
+            for (float i = 0; i < cooldown; i += .1f)
+            {
+                percent += (1 / (cooldown * 10));
                 icon.GetComponent<Image>().fillAmount = percent;
                 yield return new WaitForSeconds(.1f);
             }
